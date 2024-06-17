86adbe742b75a6597263376dc42a96cf2613e511
holgermueller <9jholger@gmail.com>
2024-06-17 11:45:06 -0400

    error handling works

--- index.js
+++ index.js
@@ -4,11 +4,34 @@ let firstName = document.getElementById("firstName");
 let lastName = document.getElementById("lastName");
 let email = document.getElementById("email");
 let message = document.getElementById("message");
-let checked = document.getElementById("consent").checked;
-let queryType = document.querySelectorAll('input[name="query"]:checked').value;
+
+let radioVal;
+
+const radios = document.querySelectorAll('input[name="query"]');
+radios.forEach((radio) => {
+  radio.addEventListener("click", function () {
+    radioVal = radio.value;
+    return radioVal;
+  });
+});
 
 const submitForm = () => {
-  let firstNameValue = queryType;
+  //Check if form is valid
+  let checked = document.getElementById("consent").checked;
 
-  console.log(firstNameValue);
+  if (firstName.value === "") {
+    document.getElementById("firstNameError").style.display = "block";
+  } else if (lastName.value === "") {
+    document.getElementById("lastNameError").style.display = "block";
+  } else if (email.value === "") {
+    document.getElementById("emailError").style.display = "block";
+  } else if (radioVal === undefined) {
+    document.getElementById("fieldSetError").style.display = "block";
+  } else if (message.value === "") {
+    document.getElementById("messageError").style.display = "block";
+  } else if (checked === false) {
+    document.getElementById("consentError").style.display = "block";
+  } else {
+    console.log("success");
+  }
 };
