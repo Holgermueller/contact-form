afc6f5ec92d4a1687a9b8a5f8b3be437075d77b7
holgermueller <9jholger@gmail.com>
2024-06-16 21:41:46 -0400

    got some form values to log

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 "use strict";
 
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let email = document.getElementById("email");
+let message = document.getElementById("message");
+let checked = document.getElementById("consent").checked;
+let queryType = document.querySelectorAll('input[name="query"]:checked').value;
+
 const submitForm = () => {
-  console.log("click");
+  let firstNameValue = queryType;
+
+  console.log(firstNameValue);
 };
